b4533d7fb7d3d2f79f165b999a72713ab04950b1
Alisson <alisson.arque@gmail.com>
2024-09-25 13:44:30 -0300

    criando o hero e importando a font Aenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
-// Importando gulp e sass
+// Importando gulp, sass e imagemin
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Função para processar os arquivos Sass
 function styles() {
     return gulp.src('./src/styles/*.scss') // Fonte dos arquivos .scss
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Compila o Sass e mostra erros
+        .pipe(sass({ outputStyle: 'compressed' })) // Compila o Sass e mostra erros
         .pipe(gulp.dest('./dist/css')); // Destino dos arquivos CSS
 }
 
-exports.default = styles;
+// Função para otimizar as imagens
+function images() {
+    return gulp.src('./src/images/**/*') // Fonte das imagens
+        .pipe(imagemin()) // Otimiza as imagens
+        .pipe(gulp.dest('./dist/images')); // Destino das imagens otimizadas
+}
+
+// Função de observação para mudanças em Sass e imagens
+exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images)
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
+
