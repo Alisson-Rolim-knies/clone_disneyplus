63334acef9c490bc14839825d44b1751f5a5d079
Alisson <alisson.arque@gmail.com>
2024-10-02 15:27:46 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 // Função para processar os arquivos Sass
 function styles() {
@@ -18,10 +25,10 @@ function images() {
 }
 
 // Função de observação para mudanças em Sass e imagens
-exports.default = gulp.parallel(styles, images);
-exports.default = gulp.parallel(styles, images)
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
